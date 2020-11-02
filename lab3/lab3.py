f0619237bde702952058d58998e79da52fe03906
logy8th <naufal.nirwansyah8@gmail.com>
2020-11-02 19:50:33 +0700

    add lab3

---
+++ lab3/lab3.py
@@ -0,0 +1,21 @@
+print("Hello, Nau's here")
+print("I'm learning Python Language")
+
+a = 7
+b = 2
+print ("variable a=",a)
+print ("variable b=",b)
+print ("hasil penjumlahan a + b= ", a+b)
+
+a=int(input("Masukkan Nilai a:"))
+b=int(input("Masukkan Nilai b:"))
+print("Variable a=",a)
+print("Variable b=",b)
+print("Hasil Penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil pejumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
+
